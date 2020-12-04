7633dcac422eed035aa18e1f906ad5f6143bbffb
Bruno Tostes <btfjulio@hotmail.com>
2020-12-04 10:22:02 -0300

    add travis ci config

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "fullname should work with user" do
+    user = User.new(first_name: 'BRUNO', last_name: 'TOSTES')
+    assert_equal "Bruno Tostes", user.fullname
+  end
 end
